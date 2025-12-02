38724868af8fa290ecc099a92b1c14828801ae3c
jamie <hi@jamie.to>
2025-12-02 16:40:56 -0500

    feat: add custom title bar color (editing in the future)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,12 +2,52 @@
 
 use tauri::{
 	menu::{Menu, MenuItem},
-	tray::TrayIconBuilder
+	tray::TrayIconBuilder,
+  TitleBarStyle,
+  WebviewUrl,
+  WebviewWindowBuilder
 };
 
 pub fn run() {
     tauri::Builder::default()
 		.setup(|app| {
+      let win_builder =
+        WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+          .title("Transparent Titlebar Window")
+          .inner_size(800.0, 600.0);
+
+      // set transparent title bar only when building for macOS
+      #[cfg(target_os = "macos")]
+      let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+
+      let window = win_builder.build().unwrap();
+
+      // set background color only when building for macOS
+      #[cfg(target_os = "macos")]
+      {
+        use cocoa::appkit::{NSColor, NSWindow};
+        use cocoa::base::{id, nil};
+
+        let ns_window = window.ns_window().unwrap() as id;
+        unsafe {
+          // let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+          //     nil,
+          //     0.0 / 255.0,
+          //     0.0 / 255.0,
+          //     0.0 / 255.0,
+          //     1.0,
+          // );
+          let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+              nil,
+              0.0 / 255.0,
+              0.0 / 255.0,
+              0.0 / 255.0,
+              1.0,
+          );
+          ns_window.setBackgroundColor_(bg_color);
+        }
+      }
+
 			let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
 			let menu = Menu::with_items(app, &[&quit_i])?;
 
