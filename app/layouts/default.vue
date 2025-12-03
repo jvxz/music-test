2c028037f735d46d87ace3990ab50d31b6aa21d2
jamie <hi@jamie.to>
2025-12-03 13:34:44 -0500

    feat: add mockup default layout with styles + resizable panels

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,72 @@
-<template>
-  <div>
-    <SiteNavbar />
+<script lang="ts" setup>
+const { layoutPanels } = useLayoutPanels()
 
-    <UContainer>
-      <slot />
-    </UContainer>
+const sidebarButtons = [
+  {
+    icon: 'tabler:playlist',
+    name: 'Playlists',
+    slug: 'playlists',
+  },
+  {
+    icon: 'tabler:music',
+    name: 'Tracks',
+    slug: 'tracks',
+  },
+  {
+    icon: 'tabler:user',
+    name: 'Artists',
+    slug: 'artists',
+  },
+
+] as const
+</script>
+
+<template>
+  <div class="h-screen">
+    <SplitterGroup
+      direction="horizontal"
+      class="flex size-full"
+      @layout="layoutPanels = $event"
+    >
+      <SplitterPanel
+        :max-size="35"
+        :min-size="12.5"
+        class="flex shrink-0 flex-col border-r"
+        :default-size="layoutPanels[0]"
+      >
+        <div class="flex h-16 items-center justify-center border-b">
+          music-player
+        </div>
+        <div class="flex flex-col gap-4 p-4">
+          <UInput placeholder="Search library" />
+          <div class="flex flex-col gap-1">
+            <UButton
+              v-for="button in sidebarButtons"
+              :key="button.slug"
+              variant="ghost"
+              class="justify-start"
+            >
+              <Icon :name="button.icon" class="size-4" />
+              <span>{{ button.name }}</span>
+              <div class="grow" />
+              <Icon name="tabler:chevron-down" class="size-4" />
+            </UButton>
+          </div>
+        </div>
+      </SplitterPanel>
+      <SplitterResizeHandle />
+      <SplitterPanel class="flex-1">
+        <slot />
+      </SplitterPanel>
+      <SplitterResizeHandle />
+      <SplitterPanel
+        :max-size="35"
+        :min-size="12.5"
+        :default-size="layoutPanels[2]"
+        class="shrink-0 border-l"
+      >
+        Sidebar
+      </SplitterPanel>
+    </SplitterGroup>
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <p>Index page</p>
+  <div>Test</div>
 </template>
