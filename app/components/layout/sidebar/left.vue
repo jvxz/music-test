8369e4edd4314892f872c18974b444ee3d487c3f
jamie <hi@jamie.to>
2025-12-04 14:24:45 -0500

    feat: extract left sidebar to component file, add folder functionality

---
+++ app/components/layout/sidebar/left.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+const sidebarButtons = [
+  {
+    icon: 'tabler:playlist',
+    name: 'Playlists',
+    slug: 'playlists',
+  },
+  {
+    icon: 'tabler:music',
+    name: 'Tracks',
+    slug: 'tracks',
+  },
+  {
+    icon: 'tabler:user',
+    name: 'Artists',
+    slug: 'artists',
+  },
+
+] as const
+</script>
+
+<template>
+  <div>
+    <div class="flex h-16 items-center justify-center border-b">
+      music-player
+    </div>
+    <div class="flex flex-col gap-4 p-4">
+      <UInput placeholder="Search library" />
+      <div class="flex flex-col gap-1">
+        <CollapsibleRoot
+          v-for="button in sidebarButtons"
+          :key="button.slug"
+        >
+          <CollapsibleTrigger as-child>
+            <UButton
+              variant="ghost"
+              class="group w-full justify-start"
+            >
+              <Icon name="tabler:chevron-right" class="size-4 group-data-[state=open]:rotate-90" />
+              <Icon
+                name="tabler:folder"
+                class="size-4"
+              />
+              <span>{{ button.name }}</span>
+            </UButton>
+          </CollapsibleTrigger>
+          <CollapsibleContent class="overflow-hidden pl-4.5 data-[state=closed]:animate-slide-up data-[state=open]:animate-slide-down">
+            <div class="p-1">
+              <UButton
+                v-for="i in 3"
+                :key="i"
+                variant="ghost"
+                class="w-full justify-start"
+              >
+                <Icon :name="button.icon" class="size-4" />
+                {{ button.name }}
+              </UButton>
+            </div>
+          </CollapsibleContent>
+        </CollapsibleRoot>
+      </div>
+    </div>
+  </div>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,24 +1,5 @@
 <script lang="ts" setup>
 const { layoutPanels } = useLayoutPanels()
-
-const sidebarButtons = [
-  {
-    icon: 'tabler:playlist',
-    name: 'Playlists',
-    slug: 'playlists',
-  },
-  {
-    icon: 'tabler:music',
-    name: 'Tracks',
-    slug: 'tracks',
-  },
-  {
-    icon: 'tabler:user',
-    name: 'Artists',
-    slug: 'artists',
-  },
-
-] as const
 </script>
 
 <template>
@@ -34,25 +15,7 @@ const sidebarButtons = [
         class="flex shrink-0 flex-col border-r"
         :default-size="layoutPanels[0]"
       >
-        <div class="flex h-16 items-center justify-center border-b">
-          music-player
-        </div>
-        <div class="flex flex-col gap-4 p-4">
-          <UInput placeholder="Search library" />
-          <div class="flex flex-col gap-1">
-            <UButton
-              v-for="button in sidebarButtons"
-              :key="button.slug"
-              variant="ghost"
-              class="justify-start"
-            >
-              <Icon :name="button.icon" class="size-4" />
-              <span>{{ button.name }}</span>
-              <div class="grow" />
-              <Icon name="tabler:chevron-down" class="size-4" />
-            </UButton>
-          </div>
-        </div>
+        <LayoutSidebarLeft />
       </SplitterPanel>
       <SplitterResizeHandle />
       <SplitterPanel class="flex-1">
