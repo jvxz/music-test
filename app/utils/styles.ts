6f23f3a9eb31144188a5e787f5385b54941307cb
jamie <hi@jamie.to>
2025-12-04 15:07:19 -0500

    refactor: edit global styles

--- app/utils/styles.ts
+++ app/utils/styles.ts
@@ -1,10 +1,10 @@
 const interactiveStyles = {
-  base: 'font-medium focus-visible:border-primary/50 select-none focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/60 aria-invalid:ring-danger/20 aria-invalid:border-danger dark:aria-invalid:ring-danger/40 shrink-0 gap-2 rounded text-base whitespace-nowrap outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4 transition-transform duration-100',
+  base: 'font-medium focus-visible:border-primary/50 select-none focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/60 aria-invalid:ring-danger/20 aria-invalid:border-danger dark:aria-invalid:ring-danger/40 shrink-0 gap-2 rounded text-base whitespace-nowrap outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4 transition-transform duration-150',
   size: {
-    default: 'h-8 px-3 py-1.5 text-sm',
-    icon: 'aspect-square size-8',
+    default: 'h-7 px-2.25 py-1.5 text-sm',
+    icon: 'aspect-square size-7',
     lg: 'h-10 px-5 text-base text-lg',
-    sm: 'h-7 px-2.5 py-1 text-xs',
+    sm: 'h-6.5 px-2.25 py-0.75 text-xs',
   },
   variant: {
     danger:
@@ -33,17 +33,18 @@ const popoverStyles = {
   content: [
     staticStyles.base,
     staticStyles.variant.default,
-    'duration-100 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-52 overflow-hidden p-1 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:scale-[97.5%]',
+    // 'duration-150 z-50 min-w-52 overflow-hidden p-1 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:scale-[97.5%]',
+    'duration-150 z-50 min-w-52 overflow-hidden p-1 ',
   ],
   item: [
     interactiveStyles.base,
     interactiveStyles.variant.ghost,
     interactiveStyles.size.default,
-    'focus:bg-muted focus:text-accent-foreground [&_svg:not([class*=\'text-\'])]:text-muted-foreground [&_svg:not([class*=\'size-\'])]:size-12 data-[variant=danger]:text-danger-foreground data-[variant=danger]:focus:bg-danger/10 data-[variant=danger]:focus:text-danger-foreground data-[variant=danger]:*:[svg]:!text-danger-foreground dark:data-[variant=danger]:focus:bg-danger/40 relative flex cursor-default items-center p-1 px-2 text-sm outline-hidden transition-all select-none focus-visible:ring-0 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+    'duration-0 focus:bg-muted focus:text-accent-foreground [&_svg:not([class*=\'text-\'])]:text-muted-foreground [&_svg:not([class*=\'size-\'])]:size-12 data-[variant=danger]:text-danger-foreground data-[variant=danger]:focus:bg-danger/10 data-[variant=danger]:focus:text-danger-foreground data-[variant=danger]:*:[svg]:!text-danger-foreground dark:data-[variant=danger]:focus:bg-danger/40 relative flex cursor-default items-center p-1 px-2 text-sm outline-hidden transition-all select-none focus-visible:ring-0 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0',
   ],
 }
 
 const overlayStyles
-  = 'duration-100 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 fixed inset-0 z-50 bg-overlay backdrop-blur-xs'
+  = 'duration-150 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 fixed inset-0 z-50 bg-overlay backdrop-blur-xs'
 
 export { interactiveStyles, overlayStyles, popoverStyles, staticStyles }
