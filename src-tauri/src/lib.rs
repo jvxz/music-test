597014181eeaa9297b65895303440e6bd38a7e0d
jamie <hi@jamie.to>
2025-12-05 17:00:22 -0500

    refactor: fix deprecation issues, remove unused crates

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use tauri::{
   menu::{Menu, MenuItem},
   tray::TrayIconBuilder,
-  TitleBarStyle, WebviewUrl, WebviewWindowBuilder,
+  WebviewUrl, WebviewWindowBuilder,
 };
 
 use crate::files::read::FileEntry;
@@ -37,30 +37,7 @@ pub async fn run() {
         .inner_size(800.0, 600.0)
         .focused(false);
 
-      // set transparent title bar only when building for macOS
-      #[cfg(target_os = "macos")]
-      let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
-
-      let window = win_builder.build().unwrap();
-
-      // set background color and prevent focus stealing on macOS
-      #[cfg(target_os = "macos")]
-      {
-        use cocoa::appkit::{NSColor, NSWindow};
-        use cocoa::base::{id, nil};
-
-        let ns_window = window.ns_window().unwrap() as id;
-        unsafe {
-          let bg_color = NSColor::colorWithRed_green_blue_alpha_(
-            nil,
-            0.0 / 255.0,
-            0.0 / 255.0,
-            0.0 / 255.0,
-            1.0,
-          );
-          ns_window.setBackgroundColor_(bg_color);
-        }
-      }
+      win_builder.build().unwrap();
 
       let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
       let menu = Menu::with_items(app, &[&quit_i])?;
