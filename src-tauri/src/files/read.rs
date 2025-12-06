5d816019da66db94561fe765d97aca363b1fa813
jamie <hi@jamie.to>
2025-12-06 12:50:39 -0500

    refactor: replace info logs with debug

--- src-tauri/src/files/read.rs
+++ src-tauri/src/files/read.rs
@@ -1,5 +1,5 @@
 use dashmap::DashMap;
-use log::info;
+use log::debug;
 use serde::Serialize;
 use specta::Type;
 use std::fs::read_dir;
@@ -28,14 +28,12 @@ pub async fn read_folder(path: String) -> Result<Arc<Vec<FileEntry>>, String> {
   if let Some(cached_dir) = STATE.get(&path) {
     let data = cached_dir.value();
 
-    info!("Time taken to read cache: {:?}", start.elapsed());
+    debug!("Time taken to read cache: {:?}", start.elapsed());
 
     return Ok(data.clone());
   };
 
   let entires = read_dir(&path).expect("Failed to read downloads folder");
-
-  info!("Reading folder: {}", &path);
 
   let file_entires = entires
     .filter_map(|e| e.ok())
@@ -67,7 +65,7 @@ pub async fn read_folder(path: String) -> Result<Arc<Vec<FileEntry>>, String> {
     })
     .collect::<Vec<FileEntry>>();
 
-  info!("Time taken to read folder: {:?}", start.elapsed());
+  debug!("Time taken to read folder: {:?}", start.elapsed());
 
   let data = Arc::new(file_entires);
 
