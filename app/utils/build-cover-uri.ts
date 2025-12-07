20e5e6bd0c7f86479b7cdc730f9088e68c38fb91
jamie <hi@jamie.to>
2025-12-07 17:03:17 -0500

    fix: fix cover uri not working for other platforms

--- app/utils/build-cover-uri.ts
+++ app/utils/build-cover-uri.ts
@@ -1,1 +1,7 @@
-export const buildCoverUri = createUnrefFn((filePath: string, mode: 'thumbnail' | 'full' = 'thumbnail') => `cover-${mode}://localhost/${encodeURIComponent(filePath)}`)
+export const buildCoverUri = createUnrefFn((filePath: string, mode: 'thumbnail' | 'full' = 'thumbnail') => {
+  const os = useTauriOsPlatform()
+
+  const prefix = ['windows'].includes(os) ? `http://cover-${mode}.localhost` : `cover-${mode}://localhost`
+
+  return `${prefix}/${encodeURIComponent(filePath)}`
+})
