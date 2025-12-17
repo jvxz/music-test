5aa900c6f8d69b62b1cfa23e60a187b1fbb7da6d
jamie <hi@jamie.to>
2025-12-17 13:50:55 -0500

    fix: fix cover art in bottom left from showing the selected track, not the playing one

--- app/components/layout/panel/cover-art.vue
+++ app/components/layout/panel/cover-art.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
-const { selectedTrack } = useTrackSelection()
+  const { currentTrack } = usePlayback()
 </script>
 
 <template>
   <div class="grid size-full place-items-center">
     <img
-      v-if="selectedTrack?.path"
-      :data-cover-missing="!selectedTrack?.tags.APIC ? '' : undefined"
-      :src="selectedTrack?.full_uri"
+      v-if="currentTrack?.path"
+      :data-cover-missing="!currentTrack?.tags.APIC ? '' : undefined"
+      :src="currentTrack?.full_uri"
       class="h-full w-full object-contain data-cover-missing:scale-75 data-cover-missing:opacity-80 dark:data-cover-missing:invert"
     />
   </div>
