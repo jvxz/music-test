231572437243aae6a47bdfd44d08d1852de1e16a
jamie <hi@jamie.to>
2025-12-16 19:23:05 -0500

    feat: add custom vue directive for disabling auto correct

---
+++ app/plugins/autocorrect.ts
@@ -0,0 +1,16 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive('no-autocorrect', {
+    mounted(el: HTMLElement) {
+      const inputs = el.matches('input, textarea, [contenteditable]')
+        ? [el]
+        : el.querySelectorAll('input, textarea, [contenteditable]')
+
+      inputs.forEach((input) => {
+        input.setAttribute('autocomplete', 'off')
+        input.setAttribute('autocorrect', 'off')
+        input.setAttribute('autocapitalize', 'off')
+        input.setAttribute('spellcheck', 'false')
+      })
+    },
+  })
+})
