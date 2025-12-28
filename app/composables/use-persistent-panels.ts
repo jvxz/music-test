7888746dc19bf99ba6dd64c4257286d403447a1a
jamie <hi@jamie.to>
2025-12-28 17:40:36 -0500

    refactor: migrate off of db plugin + restructure tauri impl

--- app/composables/use-persistent-panels.ts
+++ app/composables/use-persistent-panels.ts
@@ -1,11 +1,11 @@
 export function usePersistentPanels(key: string, defaultPanels: number[]) {
-  const { $tauri } = useNuxtApp()
+  const tauri = useTauri()
 
   const storeLayoutPanelsState = useDebounceFn(async (panels: number[]) => {
-    await $tauri.store.set(`panels:${key}`, panels)
+    await tauri.store.set(`panels:${key}`, panels)
   })
 
-  const layoutPanels = refWithControl<number[]>($tauri.prefs.get(`panels:${key}`) as number[] ?? defaultPanels, {
+  const layoutPanels = refWithControl<number[]>(tauri.prefs.get(`panels:${key}`) as number[] ?? defaultPanels, {
     onChanged: storeLayoutPanelsState,
   })
 
--- app/utils/$db.ts
+++ app/utils/$db.ts
@@ -1,1 +1,12 @@
-export const $db = () => useNuxtApp().$db
+import { appDataDir } from '@tauri-apps/api/path'
+import Database from '@tauri-apps/plugin-sql'
+import { Kysely } from 'kysely'
+import { TauriSqliteDialect } from 'kysely-dialect-tauri'
+
+const db = new Kysely<DB>({
+  dialect: new TauriSqliteDialect({
+    database: async prefix => Database.load(`${prefix}${await appDataDir()}/swim.db`),
+  }),
+})
+
+export const $db = () => db
