851610e00a979874837111603076c5d041a5f90f
jamie <hi@jamie.to>
2026-01-02 15:33:34 -0500

    feat: add lastfm network fetch util with schemas

---
+++ app/utils/lastfm/schema.ts
@@ -0,0 +1,22 @@
+export const LastFmUserSchema = z.object({
+  age: z.string(),
+  album_count: z.string(),
+  artist_count: z.string(),
+  bootstrap: z.string(),
+  country: z.string(),
+  gender: z.string(),
+  image: z.array(z.object({ '#text': z.string(), 'size': z.string() })),
+  name: z.string(),
+  playcount: z.string(),
+  playlists: z.string(),
+  realname: z.string(),
+  registered: z.object({ '#text': z.number(), 'unixtime': z.string() }),
+  subscriber: z.string(),
+  track_count: z.string(),
+  type: z.string(),
+  url: z.string(),
+})
+
+export const LastFmUserGetInfoResponseSchema = z.object({
+  user: LastFmUserSchema,
+})
