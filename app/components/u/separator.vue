e334c88909db0990564d3fa691a3a6975a3da8e1
jamie <hi@jamie.to>
2026-01-05 16:55:26 -0500

    feat: add separator component

---
+++ app/components/u/separator.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <Separator v-bind="delegatedProps" :class="cn('h-px bg-border', props.class)" />
+</template>
