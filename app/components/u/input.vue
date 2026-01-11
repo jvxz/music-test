09f29352925d1baf1edb5bc6d09e888059d05955
jamie <hi@jamie.to>
2026-01-11 16:50:17 -0500

    refactor: edit switch + input components

--- app/components/u/input.vue
+++ app/components/u/input.vue
@@ -23,6 +23,6 @@ const modelValue = useVModel(props, 'modelValue', emits, {
     v-bind="$attrs"
     v-model="modelValue"
     data-1p-ignore
-    :class="cn(staticStyles.base, interactiveStyles.size.default, staticStyles.variant.default, 'flex w-full min-w-0 cursor-text truncate py-1 font-medium selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:text-foreground placeholder:text-muted-foreground focus-visible:ring-0 md:text-sm', props.class)"
+    :class="cn(staticStyles.base, interactiveStyles.size.default, staticStyles.variant.default, 'flex w-full min-w-0 cursor-text truncate py-1 font-sans font-medium selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:text-foreground placeholder:text-muted-foreground focus-visible:ring-0 md:text-sm', props.class)"
   >
 </template>
