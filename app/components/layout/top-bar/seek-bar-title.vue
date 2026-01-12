d776186d0eae77a90cc0b9f84021711e74e16e02
jamie <hi@jamie.to>
2026-01-12 14:08:15 -0500

    feat: allow file dragging from seekbar title

--- app/components/layout/top-bar/seek-bar-title.vue
+++ app/components/layout/top-bar/seek-bar-title.vue
@@ -1,13 +1,31 @@
 <script lang="ts" setup>
-defineProps<{
+import { resolveResource } from '@tauri-apps/api/path'
+
+const props = defineProps<{
   currentTrack: CurrentPlayingTrack | null
 }>()
+
+async function handleDragStart() {
+  if (!props.currentTrack)
+    return
+
+  const icon = await resolveResource('icons/file-light.svg')
+
+  await startDrag({
+    icon,
+    item: [props.currentTrack.path],
+  })
+}
 </script>
 
 <template>
   <UContextMenu>
     <UContextMenuTrigger as-child>
-      <div class="flex h-9 cursor-default flex-col text-center">
+      <div
+        class="flex h-9 cursor-default flex-col text-center"
+        draggable="true"
+        @dragstart.prevent="handleDragStart"
+      >
         <p class="truncate text-sm">
           {{ currentTrack?.tags.TIT2 ?? currentTrack?.name }}
         </p>
