ecb857f4e3a7e9e38dee23b76c48536072441d1b
jamie <hi@jamie.to>
2026-01-23 16:41:59 -0500

    feat: add checkbox form component

---
+++ app/components/form/checkbox.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps<{
+  id: string
+  label: string
+}>()
+
+const modelValue = defineModel<boolean>()
+</script>
+
+<template>
+  <div class="flex items-center gap-2" v-bind="$attrs">
+    <UCheckbox :id v-model:model-value="modelValue" />
+    <ULabel for="roundedCorners">
+      Rounded corners
+    </ULabel>
+  </div>
+</template>
