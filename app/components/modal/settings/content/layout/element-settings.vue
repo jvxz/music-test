46af36a3adad879747c70d533ced71fefa1ff889
jamie <hi@jamie.to>
2026-01-23 21:35:16 -0500

    chore: lint

--- app/components/modal/settings/content/layout/element-settings.vue
+++ app/components/modal/settings/content/layout/element-settings.vue
@@ -18,7 +18,7 @@ const ElementSettings = computedWithControl(element, () => defineAsyncComponent<
 <template>
   <UDialogRoot :open="!!element" @update:open="!$event && (element = undefined)">
     <!-- <UDialogContent class="flex flex-col sm:w-full lg:w-[850px] xl:w-[1050px] 2xl:w-[1150px]"> -->
-    <UDialogContent class="flex flex-col w-2xl">
+    <UDialogContent class="flex w-2xl flex-col">
       <UDialogHeader>
         <UDialogTitle>
           {{ layoutPanelElements.find(e => e.key === element)?.label }}
