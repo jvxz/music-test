60b7af2d2a8e106f4e89b7a327d8f482e38dc590
jamie <hi@jamie.to>
2026-01-23 21:34:23 -0500

    fix: fix form checkbox component

--- app/components/form/checkbox.vue
+++ app/components/form/checkbox.vue
@@ -1,17 +1,23 @@
 <script lang="ts" setup>
 defineProps<{
-  id: string
+  disabled?: boolean
   label: string
 }>()
+
+const id = useId()
 
 const modelValue = defineModel<boolean>()
 </script>
 
 <template>
   <div class="flex items-center gap-2" v-bind="$attrs">
-    <UCheckbox :id v-model:model-value="modelValue" />
-    <ULabel for="roundedCorners">
-      Rounded corners
+    <UCheckbox
+      :id
+      v-model:model-value="modelValue"
+      :disabled
+    />
+    <ULabel :for="id" :disabled>
+      {{ label }}
     </ULabel>
   </div>
 </template>
