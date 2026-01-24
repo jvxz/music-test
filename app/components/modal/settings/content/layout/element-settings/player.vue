7cdf37dc518fd6ff7f645fe37e22ac45092577b1
jamie <hi@jamie.to>
2026-01-23 21:36:11 -0500

    feat: add player element settings content

---
+++ app/components/modal/settings/content/layout/element-settings/player.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const { getElementSettings } = useLayout()
+const elementSettings = getElementSettings('player')
+</script>
+
+<template>
+  <FormCheckbox
+    v-model:model-value="elementSettings.showTrackCover"
+    label="Show track cover"
+  />
+  <FormCheckbox
+    v-model:model-value="elementSettings.roundTrackCover"
+    :disabled="!elementSettings.showTrackCover"
+    label="Round track cover corners"
+  />
+</template>
