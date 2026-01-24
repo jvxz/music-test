3c931627e54edb9d11d39d9ac237ea9fe4e12468
jamie <hi@jamie.to>
2026-01-23 21:34:38 -0500

    refactor: edit cover art layout panel component

--- app/components/layout/panel/cover-art.vue
+++ app/components/layout/panel/cover-art.vue
@@ -1,20 +1,24 @@
 <script lang="ts" setup>
+import type { ImgHTMLAttributes } from 'vue'
+
 type ClassAttributeNames = 'img' | 'noCoverText' | 'root'
 
 const props = defineProps<{
   noCoverText?: string
-  classes?: Partial<Record<ClassAttributeNames, string>>
+  classes?: Partial<Record<ClassAttributeNames, string | string[]>>
+  img?: ImgHTMLAttributes
 }>()
 
 const { currentTrack } = usePlayback()
 </script>
 
 <template>
-  <div :class="cn('flex size-full items-center justify-center', props.classes?.root)">
+  <div :class="cn('aspect-square items-center justify-center', props.classes?.root)">
     <img
       v-if="currentTrack && currentTrack.tags.APIC"
+      v-bind="img"
       :src="currentTrack?.full_uri"
-      :class="cn('h-full object-contain', props.classes?.img)"
+      :class="cn('h-full object-cover', props.classes?.img)"
     />
     <div
       v-else
