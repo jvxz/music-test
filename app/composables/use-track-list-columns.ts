cb3865639dfb24cdb6f93a11f1c46fef08461bd7
jamie <hi@jamie.to>
2026-01-27 13:56:19 -0500

    refactor: edit default column sizing state

--- app/composables/use-track-list-columns.ts
+++ app/composables/use-track-list-columns.ts
@@ -1,1 +1,10 @@
-export const useTrackListColumns = createSharedComposable(() => usePersistentPanels('track-list-columns', [5, 5, 25, 20, 20, 10, 15]))
+export const useTrackListColumns = createSharedComposable(() => usePersistentPanels('track-list-columns', [
+  3,
+  1.5,
+  34.0888869203,
+  15.1599593363,
+  15.0211805522,
+  8.5755556775,
+  14.3210841804,
+  8.3333333333,
+]))
