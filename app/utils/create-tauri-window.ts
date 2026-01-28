20184d0a744fb9eb6925bb6718aaedbaa5d192aa
jamie <hi@jamie.to>
2026-01-28 16:22:34 -0500

    refactor: make url required in tauri window creator util

--- app/utils/create-tauri-window.ts
+++ app/utils/create-tauri-window.ts
@@ -2,7 +2,11 @@ import type { WebviewLabel, WebviewOptions } from '@tauri-apps/api/webview'
 import type { WindowOptions } from '@tauri-apps/api/window'
 import { WebviewWindow } from '@tauri-apps/api/webviewWindow'
 
-export function createTauriWindow(label: WebviewLabel, options?: Omit<WebviewOptions, 'x' | 'y' | 'width' | 'height'> & WindowOptions) {
+type Options = Omit<WebviewOptions, 'x' | 'y' | 'width' | 'height'> & WindowOptions & {
+  url: string
+}
+
+export function createTauriWindow(label: WebviewLabel, options: Options) {
   const { getSettingValue } = useSettings()
   const backgroundColor = getSettingValue('appearance.token.background')
 
