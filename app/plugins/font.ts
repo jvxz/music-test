2d321a9e6947e01a2d7dbeef03dcb74564e02cd1
jamie <hi@jamie.to>
2026-02-02 18:15:09 -0500

    refactor: edit plugins to comply with new types

--- app/plugins/font.ts
+++ app/plugins/font.ts
@@ -2,16 +2,13 @@ export default defineNuxtPlugin({
   dependsOn: ['tauri'],
   parallel: true,
   setup: () => {
-    const { getSettingValueRef } = useSettings()
-    const appearanceFont = getSettingValueRef('appearance.font.name')
-    const appearanceFontWeight = getSettingValueRef('appearance.font.weight')
-    const appearanceFontVariant = getSettingValueRef('appearance.font.variant')
+    const settings = useSettings()
 
     useStyleTag(computed(() => `
       body {
-        font-family: ${appearanceFont.value};
-        font-weight: ${appearanceFontWeight.value};
-        font-style: ${appearanceFontVariant.value};
+        font-family: ${settings.appearance.font.name};
+        font-weight: ${settings.appearance.font.weight};
+        font-style: ${settings.appearance.font.variant};
       }
     `))
   },
--- app/plugins/theme.ts
+++ app/plugins/theme.ts
@@ -2,16 +2,16 @@ export default defineNuxtPlugin({
   dependsOn: ['tauri'],
   parallel: true,
   setup: () => {
-    const { settings } = useSettings()
+    const settings = useSettings()
 
     useStyleTag(computed(() => `
       :root {
-        --background: ${settings.value['appearance.token.background']};
-        --foreground: ${settings.value['appearance.token.foreground']};
-        --primary: ${settings.value['appearance.token.primary']};
-        --border: ${settings.value['appearance.token.border']};
-        --card: ${settings.value['appearance.token.surface']};
-        --muted: ${settings.value['appearance.token.muted']};
+        --background: ${settings.appearance.token.background};
+        --foreground: ${settings.appearance.token.foreground};
+        --primary: ${settings.appearance.token.primary};
+        --border: ${settings.appearance.token.border};
+        --card: ${settings.appearance.token.surface};
+        --muted: ${settings.appearance.token.muted};
       }
     `))
   },
