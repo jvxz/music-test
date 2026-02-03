2ac1d1eaeaeb4191cca690839a61fdc9ab674e53
jamie <hi@jamie.to>
2026-02-03 17:24:43 -0500

    feat: create draggable components

---
+++ app/components/u/draggable/item.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import type { AcceptableValue } from 'reka-ui'
+
+export type DraggableItem<T extends NonNullable<AcceptableValue>> = Prettify<{
+  data: T
+  label: string
+  key: string
+}>
+
+defineProps<{
+  item: any
+}>()
+</script>
+
+<template>
+  <UButton
+    :id="item.key"
+    variant="ghost"
+    draggable="true"
+    class="justify-start gap-2 rounded-t-none rounded-b-none transition-none duration-0 first:rounded-t last:rounded-b active:text-foreground"
+  >
+    <Icon name="tabler:grip-vertical" class="size-3.5!" />
+    <slot />
+  </UButton>
+</template>
+
+<style scoped>
+@reference '@/assets/css/globals.css';
+
+.sortable-ghost {
+  @apply bg-muted text-foreground;
+}
+</style>
---
+++ app/components/u/draggable/label.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{
+  class?: string
+}>()
+</script>
+
+<template>
+  <ULabel :class="cn('font-medium ps-1', $props.class)">
+    <slot />
+  </ULabel>
+</template>
