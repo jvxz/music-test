21aed5c90d3fae8e7e7e8105b4b8f50336f78990
jamie <hi@jamie.to>
2026-02-03 17:24:02 -0500

    chore: automatically reveal window on app mount

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,12 @@
+<script lang="ts" setup>
+import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow'
+
+onMounted(async () => {
+  const window = getCurrentWebviewWindow()
+  await window.show()
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
