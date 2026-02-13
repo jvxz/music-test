4778cf10e1d186747bcd5dd770803096a96d3d65
jamie <hi@jamie.to>
2026-02-13 17:59:07 -0500

    refactor: edit form subtitle component

--- app/components/form/subtitle.vue
+++ app/components/form/subtitle.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="space-y-1 not-first:pt-2">
+  <div class="space-y-1 not-first:pt-2 shrink-0">
     <ULabel :class="cn('text-lg font-medium', $props.class)">
       <slot />
     </ULabel>
