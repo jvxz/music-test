9de94a11028280534ed200887ec22f5571f98414
jamie <hi@jamie.to>
2026-02-16 13:58:44 -0500

    feat: enhance metadata layout panel field component

--- app/components/layout/panel/metadata/field.vue
+++ app/components/layout/panel/metadata/field.vue
@@ -7,10 +7,30 @@ defineProps<{
 
 <template>
   <div class="flex flex-col gap-1">
-    <ULabel class="text-sm">
+    <ULabel class="shrink-0 text-sm">
       {{ ID3_MAP[id3Frame] }}
     </ULabel>
-    <UInput v-if="track && track.valid" :model-value="track.tags[id3Frame]" />
-    <UInput v-else disabled />
+    <template v-if="id3Frame === 'APIC'">
+      <CoverArt
+        v-if="track"
+        :track="$props.track"
+        :classes="{
+          root: 'rounded w-fit size-48',
+          noCoverText: 'border border-dashed border-primary',
+        }"
+      />
+    </template>
+    <LayoutPanelMetadataFieldComments
+      v-else-if="id3Frame === 'COMM'"
+      :track
+    />
+    <template v-else>
+      <UInput
+        v-if="track && track.valid"
+        :model-value="track.tags[id3Frame]"
+        style="text-transform: none"
+      />
+      <UInput v-else disabled />
+    </template>
   </div>
 </template>
