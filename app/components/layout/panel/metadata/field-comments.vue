320121e53e74e35ecd5238b134a25c9dcc85d75c
jamie <hi@jamie.to>
2026-02-16 13:56:27 -0500

    feat: add special field for comments for metadata panel

---
+++ app/components/layout/panel/metadata/field-comments.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+defineProps<{
+  track: TrackListEntry | null
+}>()
+const settings = useSettings()
+
+const height = shallowRef(settings.layout.element.metadataView.frameCommHeight)
+
+const textarea = useTemplateRef<HTMLTextAreaElement>('textarea')
+const { height: elHeight } = useElementSize(textarea)
+watchDebounced(elHeight, height => settings.layout.element.metadataView.frameCommHeight = height, { debounce: 200 })
+</script>
+
+<template>
+  <UTextarea
+    v-if="track && track.valid"
+    ref="textarea"
+    :model-value="track.tags.COMM"
+    style="text-transform: none"
+    :style="{ height: `${height}px` }"
+    class="max-h-96 min-h-16 resize-y"
+  />
+  <UTextarea
+    v-else
+    ref="textarea"
+    disabled
+    style="text-transform: none"
+    :style="{ height: `${height}px` }"
+    class="max-h-96 min-h-16 resize-y"
+  />
+</template>
