6307af2de7ab45a6236a134ed7270f115fb350c2
jamie <hi@jamie.to>
2026-02-16 13:22:21 -0500

    refactor: edit cover art components

--- app/components/cover-art.vue
+++ app/components/cover-art.vue
@@ -1,20 +1,30 @@
 <script lang="ts" setup>
+import type { ImgHTMLAttributes } from 'vue'
+
+type ClassAttributeNames = 'img' | 'noCoverText' | 'root'
+
 const props = defineProps<{
-  track: CurrentPlayingTrack | null
-  class?: string
+  noCoverText?: string
+  classes?: Partial<Record<ClassAttributeNames, string | string[]>>
+  img?: ImgHTMLAttributes
 }>()
+
+const { currentTrack } = usePlayback()
 </script>
 
 <template>
-  <img
-    v-if="track && track.tags.APIC"
-    :src="track?.full_uri"
-    :class="cn('h-full', props.class)"
-  />
-  <div
-    v-else
-    :class="cn('grid size-full place-items-center font-mono text-sm text-muted-foreground', props.class)"
-  >
-    no cover
+  <div :class="cn('flex size-full items-center justify-center overflow-hidden', props.classes?.root)">
+    <img
+      v-if="currentTrack && currentTrack.tags.APIC"
+      v-bind="img"
+      :src="currentTrack?.full_uri"
+      :class="cn('h-full object-contain', props.classes?.img)"
+    />
+    <div
+      v-else
+      :class="cn('grid aspect-square size-full place-items-center text-sm text-muted-foreground', props.classes?.noCoverText)"
+    >
+      {{ noCoverText ?? 'no cover' }}
+    </div>
   </div>
 </template>
