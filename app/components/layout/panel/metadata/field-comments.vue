f67356c6d99029aa11bdfb93220cd62141f576c4
jamie <hi@jamie.to>
2026-02-16 22:47:31 -0500

    refactor: edit comments field component for metadata panel

--- app/components/layout/panel/metadata/field-comments.vue
+++ app/components/layout/panel/metadata/field-comments.vue
@@ -9,13 +9,16 @@ const height = shallowRef(settings.layout.element.metadataView.frameCommHeight)
 const textarea = useTemplateRef<HTMLTextAreaElement>('textarea')
 const { height: elHeight } = useElementSize(textarea)
 watchDebounced(elHeight, height => settings.layout.element.metadataView.frameCommHeight = height, { debounce: 200 })
+
+const { proposedChanges } = useMetadata()
 </script>
 
 <template>
   <UTextarea
     v-if="track && track.valid"
     ref="textarea"
-    :model-value="track.tags.COMM"
+    v-no-autocorrect
+    v-model:model-value="proposedChanges.COMM"
     style="text-transform: none"
     :style="{ height: `${height}px` }"
     class="max-h-96 min-h-16 resize-y"
