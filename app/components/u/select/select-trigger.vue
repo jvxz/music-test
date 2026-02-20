a0d79b59eab7f3e1500ff318f13d2df9c32c1883
jamie <hi@jamie.to>
2026-02-20 16:37:31 -0500

    feat: enhance select trigger component

--- app/components/u/select/select-trigger.vue
+++ app/components/u/select/select-trigger.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import type { SelectTriggerProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
+import type { ButtonProps } from '../button.vue'
 import { useForwardProps } from 'reka-ui'
 
 const props = withDefaults(
-  defineProps<SelectTriggerProps & { class?: HTMLAttributes['class'], size?: 'sm' | 'default' }>(),
-  { size: 'default' },
+  defineProps<SelectTriggerProps & {
+    class?: HTMLAttributes['class']
+    size?: 'sm' | 'default'
+    variant?: ButtonProps['variant']
+    withIcon?: boolean
+  }>(),
+  { size: 'default', variant: 'soft', withIcon: true },
 )
 
 const delegatedProps = reactiveOmit(props, 'class', 'size')
@@ -19,15 +25,17 @@ const forwardedProps = useForwardProps(delegatedProps)
     v-bind="forwardedProps"
     :class="cn(
       interactiveStyles.base,
-      interactiveStyles.variant.soft,
-      interactiveStyles.size.default,
-      'flex items-center justify-between data-placeholder:text-muted-foreground *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2 pr-1.5',
+      interactiveStyles.variant[props.variant],
+      interactiveStyles.size[props.size],
+      'flex items-center justify-between data-placeholder:text-muted-foreground *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2',
+      withIcon && 'pr-1.5',
       props.class,
     )"
   >
-  <slot />
-  <SelectIcon as-child>
-    <Icon name="tabler:chevron-down" class="size-3.5!" />
-  </SelectIcon>
+    <slot />
+
+    <SelectIcon v-if="withIcon" as-child>
+      <Icon name="tabler:chevron-down" class="size-3.5!" />
+    </SelectIcon>
   </SelectTrigger>
 </template>
