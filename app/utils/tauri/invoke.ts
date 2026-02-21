361cdcebef9a471b1d9550aba2946a13be90acc2
jamie <hi@jamie.to>
2026-02-21 17:02:57 -0500

    feat: solidify $invoke + remove dev code

--- app/utils/tauri/invoke.ts
+++ app/utils/tauri/invoke.ts
@@ -1,7 +1,7 @@
 type Command = typeof commands[keyof typeof commands]
 
 function isResult<T, E>(res: unknown): res is Result<T, E> {
-  return res != null && typeof res === 'object' && 'status' in res
+  return res != null && typeof res === 'object' && 'status' in res && (res.status === 'ok' || res.status === 'error')
 }
 
 type UnwrapResult<T> = T extends Promise<Result<infer U, any>> ? U : Awaited<T>
@@ -13,10 +13,10 @@ export async function $invoke<T extends Command>(cmd: T, ...args: Parameters<T>)
   if (!res)
     return returnValue as UnwrapResult<ReturnType<T>>
 
-  if (returnValue.status === 'error')
-    throw emitError(returnValue.error)
+  if (returnValue.status === 'error') {
+    emitError(returnValue.error)
+    throw returnValue.error
+  }
 
   return returnValue.data as UnwrapResult<ReturnType<T>>
 }
-
-$invoke(commands.getCanonicalPath, 'test')
