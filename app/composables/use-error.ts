8a923522817b7b20fefc6f351e78c99c81786d9b
jamie <hi@jamie.to>
2026-02-25 15:53:45 -0500

    refactor: edit useError composable

--- app/composables/use-error.ts
+++ app/composables/use-error.ts
@@ -2,6 +2,7 @@ import type { Error } from '~/types/tauri-bindings'
 
 export const ERROR_TITLE_MAP: Record<Error['type'], string> = {
   Audio: 'Audio error',
+  Backend: 'Backend error',
   FileSystem: 'File system error',
   Id3: 'ID3 error',
   LastFm: 'Last.fm error',
@@ -11,6 +12,7 @@ export const ERROR_TITLE_MAP: Record<Error['type'], string> = {
   Stronghold: 'Stronghold error',
   Waveform: 'Waveform error',
 }
+export const ERROR_TYPES = objectKeys(ERROR_TITLE_MAP)
 
 export const errorHook = createEventHook<Error>()
-export const emitError = useThrottleFn(errorHook.trigger, 1000)
+export const emitError = errorHook.trigger
