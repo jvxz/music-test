f98a097e9cdadca4ec6fec88dbca0759689580b0
jamie <hi@jamie.to>
2026-02-27 17:30:20 -0500

    feat: create console layout element settings page

---
+++ app/components/window/settings/content/layout/element-settings/console.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const monoTokens = ['timestamp', 'message'] as const
+</script>
+
+<template>
+  <FormSubtitle>
+    Console
+  </FormSubtitle>
+  <FormPrimitive label="Use mono font for:" class="pb-2">
+    <FormCheckbox
+      v-for="token in monoTokens"
+      :key="token"
+      v-model:model-value="$settings.layout.element.console[`${token}Mono`]"
+      :value="token"
+      :label="upperFirst(token)"
+    />
+  </FormPrimitive>
+  <FormCheckbox v-model:model-value="$settings.layout.element.console.wrapText" label="Wrap text" />
+</template>
