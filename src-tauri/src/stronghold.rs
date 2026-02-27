f520b5a6f7c3b51585fba46417d6f2acb8500ec0
jamie <hi@jamie.to>
2026-02-27 13:38:11 -0500

    refactor: refactor stronghold logic to use blocking thread

--- src-tauri/src/stronghold.rs
+++ src-tauri/src/stronghold.rs
@@ -1,79 +1,93 @@
 use iota_stronghold::ClientError;
-use std::sync::Mutex;
-use tauri::{AppHandle, Manager};
+use tauri::{async_runtime::spawn_blocking, AppHandle, Manager, State};
 use tauri_plugin_stronghold::stronghold::Stronghold;
+use tokio::sync::Mutex;
 use uuid::Uuid;
 
 use crate::error::{Error, Result};
 
-static STRONGHOLD_CLIENT: Mutex<Option<iota_stronghold::Client>> = Mutex::new(None);
+static STRONGHOLD_CLIENT: Mutex<Option<iota_stronghold::Client>> = Mutex::const_new(None);
+static STRONGHOLD_INIT: Mutex<bool> = Mutex::const_new(false);
 
-pub fn init_stronghold(app: AppHandle<tauri::Wry>) -> Result<()> {
+pub async fn load_stronghold(app: &AppHandle<tauri::Wry>) -> Result<State<'_, Stronghold>> {
+  let mut init = STRONGHOLD_INIT.lock().await;
+
   let stronghold = app.app_handle().try_state::<Stronghold>();
-  if stronghold.is_some() {
-    return Ok(());
-  }
 
-  let vault_entry =
-    keyring::Entry::new("swim", "master-key").map_err(|e| Error::Stronghold(e.to_string()))?;
+  match stronghold {
+    Some(_) => Ok::<_, Error>(()),
+    None => {
+      let app_clone = app.clone();
 
-  let vault_pw = match vault_entry.get_password() {
-    Ok(pw) => pw,
-    Err(_) => {
-      let new_pw = Uuid::new_v4().simple().to_string();
-      vault_entry
-        .set_password(new_pw.as_str())
-        .map_err(|e| Error::Stronghold(e.to_string()))?;
-      new_pw
+      spawn_blocking(move || {
+        let vault_entry = keyring::Entry::new("swim", "master-key")
+          .map_err(|e| Error::Stronghold(e.to_string()))?;
+
+        let vault_pw = match vault_entry.get_password() {
+          Ok(pw) => pw,
+          Err(_) => {
+            let new_pw = Uuid::new_v4().simple().to_string();
+            vault_entry
+              .set_password(new_pw.as_str())
+              .map_err(|e| Error::Stronghold(e.to_string()))?;
+            new_pw
+          }
+        };
+
+        let stronghold_path = app_clone
+          .path()
+          .app_local_data_dir()
+          .expect("failed to get app local data dir")
+          .join("swim.hold");
+        let stronghold = Stronghold::new(stronghold_path, vault_pw.as_bytes().to_vec())
+          .expect("failed to create stronghold");
+        app_clone.app_handle().manage(stronghold);
+
+        return Ok(());
+      })
+      .await
+      .map_err(|e| Error::Stronghold(e.to_string()))?
     }
   };
 
-  let stronghold_path = app
-    .path()
-    .app_local_data_dir()
-    .expect("failed to get app local data dir")
-    .join("swim.hold");
-  let stronghold = Stronghold::new(stronghold_path, vault_pw.as_bytes().to_vec())
-    .expect("failed to create stronghold");
-  app.app_handle().manage(stronghold);
+  *init = true;
 
-  // return Ok(stronghold);
-  return Ok(());
+  return Ok(app.state::<Stronghold>());
 }
 
-pub fn load_stronghold_client<T: AsRef<str>>(
+pub async fn load_stronghold_client(
   app_handle: AppHandle<tauri::Wry>,
-  client_name: T,
+  client_name: String,
 ) -> Result<iota_stronghold::Client> {
-  init_stronghold(app_handle.clone())?;
+  load_stronghold(&app_handle).await?;
 
   {
-    let guard = STRONGHOLD_CLIENT.lock().map_err(|_| {
-      Error::Stronghold("failed to lock stronghold client when loading".to_string())
-    })?;
+    let guard = STRONGHOLD_CLIENT.lock().await;
 
     if let Some(client) = &*guard {
       return Ok(client.clone());
     }
   }
 
-  let stronghold = app_handle.state::<Stronghold>();
-  let mut client = stronghold.load_client(client_name.as_ref());
+  let client = spawn_blocking(move || {
+    let stronghold = app_handle.state::<Stronghold>();
+    let mut client = stronghold.load_client(&client_name);
 
-  if let Err(ClientError::ClientDataNotPresent) = client {
-    client = Ok(
-      stronghold
-        .create_client(client_name.as_ref())
-        .map_err(|_| Error::Stronghold("failed to create client".to_string()))?,
-    );
-  }
+    if let Err(ClientError::ClientDataNotPresent) = client {
+      client = Ok(
+        stronghold
+          .create_client(&client_name)
+          .map_err(|_| Error::Stronghold("failed to create client".to_string()))?,
+      );
+    }
 
-  let client = client.map_err(|_| Error::Stronghold("failed to load client".to_string()))?;
+    return client.map_err(|_| Error::Stronghold("failed to load client".to_string()));
+  })
+  .await
+  .map_err(|e| Error::Stronghold(e.to_string()))??;
 
   {
-    let mut guard = STRONGHOLD_CLIENT.lock().map_err(|_| {
-      Error::Stronghold("failed to lock stronghold client when loading".to_string())
-    })?;
+    let mut guard = STRONGHOLD_CLIENT.lock().await;
     guard.replace(client.clone());
   }
 
