7f581e178f6dc3eeec86ff1626741cf57b11e7ca
jamie <hi@jamie.to>
2026-03-02 16:25:23 -0500

    refactor: refactor directives plugin to be object syntax

--- app/plugins/directives.ts
+++ app/plugins/directives.ts
@@ -1,25 +1,28 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.directive('no-autocorrect', {
-    mounted(el: HTMLElement) {
-      const inputs = el.matches('input, textarea, [contenteditable]')
-        ? [el]
-        : el.querySelectorAll('input, textarea, [contenteditable]')
+export default defineNuxtPlugin({
+  parallel: true,
+  setup: ({ vueApp }) => {
+    vueApp.directive('no-autocorrect', {
+      mounted(el: HTMLElement) {
+        const inputs = el.matches('input, textarea, [contenteditable]')
+          ? [el]
+          : el.querySelectorAll('input, textarea, [contenteditable]')
 
-      inputs.forEach((input) => {
-        input.setAttribute('autocomplete', 'off')
-        input.setAttribute('autocorrect', 'off')
-        input.setAttribute('autocapitalize', 'off')
-        input.setAttribute('spellcheck', 'false')
-      })
-    },
-  })
+        inputs.forEach((input) => {
+          input.setAttribute('autocomplete', 'off')
+          input.setAttribute('autocorrect', 'off')
+          input.setAttribute('autocapitalize', 'off')
+          input.setAttribute('spellcheck', 'false')
+        })
+      },
+    })
 
-  nuxtApp.vueApp.directive('esc-blur', {
-    mounted(el: HTMLElement) {
-      useEventListener('keydown', (e) => {
-        if (e.key === 'Escape')
-          el.blur()
-      })
-    },
-  })
+    vueApp.directive('esc-blur', {
+      mounted(el: HTMLElement) {
+        useEventListener('keydown', (e) => {
+          if (e.key === 'Escape')
+            el.blur()
+        })
+      },
+    })
+  },
 })
