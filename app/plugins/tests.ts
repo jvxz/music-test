b07c5c92341467fa399268f360a5d53386ca4e20
jamie <hi@jamie.to>
2026-03-14 22:06:08 -0400

    chore(test): attempt to fix mockup e2e test

--- app/plugins/tests.ts
+++ app/plugins/tests.ts
@@ -3,9 +3,8 @@ import { invoke } from '@tauri-apps/api/core'
 export default defineNuxtPlugin({
   parallel: true,
   setup: () => {
-    if (!import.meta.test)
-      return
-
+    // provide the invoke function to the window object
+    // for e2e tests to use
     (window as any).__TAURI_INVOKE__ = invoke
   },
 })
--- test/e2e/specs/invoke.e2e.ts
+++ test/e2e/specs/invoke.e2e.ts
@@ -1,6 +1,11 @@
 import type { invoke } from '@tauri-apps/api/core'
+import type { Browser } from 'webdriverio'
 import type { FileEntry } from '../../../app/types/tauri-bindings'
-import { expect } from '@wdio/globals'
+import { browser, expect } from '@wdio/globals'
+
+declare global {
+  const browser: Browser
+}
 
 declare global {
   interface Window {
@@ -10,10 +15,17 @@ declare global {
 
 describe('tauri invoke', () => {
   it('successfully invokes a tauri command', async () => {
-    const trackData = await window.__TAURI_INVOKE__<FileEntry>('get_track_data', {
+    await browser.waitUntil(
+      async () => (await browser.execute(() => typeof window.__TAURI_INVOKE__ === 'function')) === true,
+      { interval: 100, timeout: 10000 },
+    )
+
+    const trackData = await browser.execute(async () => window.__TAURI_INVOKE__<FileEntry>('get_track_data', {
       pathString: '../fixtures/title-artist.mp3',
       refresh: false,
-    })
+    }))
+
+    console.log('______TRACK DATA______: ', trackData)
 
     expect(trackData).toBeDefined()
   })
