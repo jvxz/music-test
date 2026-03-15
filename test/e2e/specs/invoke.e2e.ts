66f7d2cd31c9f542a880377576efb6ff81201747
jamie <hi@jamie.to>
2026-03-15 13:41:12 -0400

    refactor(test): edit invoke e2e spec

--- test/e2e/specs/invoke.e2e.ts
+++ test/e2e/specs/invoke.e2e.ts
@@ -1,23 +1,18 @@
 import type { FileEntry } from '../../../app/types/tauri-bindings'
 import path from 'node:path'
 import { expect } from '@wdio/globals'
+import { getAudioFixtures } from '../utils/constants'
 import { $invoke } from '../utils/invoke'
 
 describe('tauri invoke commands', () => {
-  for (const test of [
-    {
-      artist: 'Artist',
-      filename: 'title-artist.mp3',
-      title: 'Title',
-    },
-  ]) {
-    it(`successfully invokes get_track_data command & returns correct data for ${test.filename}`, async () => {
-      const fixturePath = path.resolve(process.cwd(), 'test/fixtures', test.filename)
-      const trackData = await $invoke<FileEntry>('get_track_data', { pathString: fixturePath, refresh: false })
+  for (const test of getAudioFixtures()) {
+    it(`successfully invokes get_track_data command & returns correct data for ${path.basename(test.path)}`, async () => {
+      const trackData = await $invoke<FileEntry>('get_track_data', { pathString: test.path, refresh: false })
 
       expect(trackData?.valid).toBe(true)
-      expect(trackData?.tags.TIT2).toBe(test.title)
-      expect(trackData?.tags.TPE1).toBe(test.artist)
+
+      for (const [key, value] of Object.entries(test.tags))
+        expect(trackData?.tags[key]).toBe(value)
     })
   }
 })
