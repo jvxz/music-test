1314e21f0a6d05aef4c5a175d6f7df01662416d6
jamie <hi@jamie.to>
2026-03-15 13:40:44 -0400

    refactor(test): edit invoke test helper fn

--- test/e2e/utils/invoke.ts
+++ test/e2e/utils/invoke.ts
@@ -1,4 +1,10 @@
-export async function $invoke<T>(cmd: string, payload: Record<string, unknown>): Promise<T | null> {
+/// <reference path="../globals.d.ts" />
+
+type Serializable = string | number | boolean | null | undefined | Serializable[] | {
+  [key: string]: Serializable
+}
+
+export async function $invoke<T>(cmd: string, payload: Record<string, Serializable>): Promise<T | null> {
   return browser.execute(
     async (command: string, args: Record<string, unknown>) => {
       try {
