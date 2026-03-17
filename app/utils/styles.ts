792e0eed564c6ef50c14951776ac0c3daccf048d
jamie <hi@jamie.to>
2026-03-17 14:34:16 -0400

    chore: include unocss for ts files

--- app/utils/styles.ts
+++ app/utils/styles.ts
@@ -1,5 +1,6 @@
+// @unocss-include
 const interactiveStyles = {
-  base: 'font-medium focus-visible:border-primary/50 select-none focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/60 aria-invalid:ring-danger/20 aria-invalid:border-danger dark:aria-invalid:ring-danger/40 shrink-0 gap-2 rounded text-base whitespace-nowrap outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4 transition-transform duration-150',
+  base: 'font-medium focus-visible:border-primary/50 select-none focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/60 dark:aria-[invalid]:ring-danger/40 shrink-0 gap-2 rounded text-base whitespace-nowrap outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4 transition-transform duration-150',
   size: {
     default: 'h-7 px-2.25 py-1.5 text-sm',
     icon: 'aspect-square size-7',
@@ -16,8 +17,8 @@ const interactiveStyles = {
     outline:
     'hover:bg-muted/90 active:bg-muted/80 border bg-transparent',
     soft: 'text-muted-foreground hover:text-foreground active:text-foreground hover:bg-muted/90 active:bg-muted/80 border bg-card',
-    togglable: 'hover:bg-muted/55 data-drag-over:bg-muted/55 active:bg-muted/40 text-muted-foreground hover:text-foreground active:text-foreground',
-    toggled: 'bg-muted/90 data-drag-over:bg-muted/90 active:bg-muted/75 text-muted-foreground hover:text-foreground active:text-foreground',
+    togglable: 'hover:bg-muted/55 data-[drag-over]:bg-muted/55 active:bg-muted/40 text-muted-foreground hover:text-foreground active:text-foreground',
+    toggled: 'bg-muted/90 data-[drag-over]:bg-muted/90 active:bg-muted/75 text-muted-foreground hover:text-foreground active:text-foreground',
   },
 }
 
@@ -35,20 +36,17 @@ const popoverStyles = {
   content: [
     staticStyles.base,
     staticStyles.variant.default,
-    // 'duration-150 z-50 min-w-52 overflow-hidden p-1 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:scale-[97.5%]',
-    // 'duration-150 z-50 min-w-52 overflow-hidden p-1 bg-[hsl(from_var(--card)_h_s_calc(l*1.5))]',
     'duration-150 z-50 min-w-52 overflow-hidden p-1',
   ],
   item: [
     interactiveStyles.base,
     interactiveStyles.variant.ghost,
     interactiveStyles.size.default,
-    'duration-0 focus:bg-muted focus:text-accent-foreground text-foreground [&_svg:not([class*=\'text-\'])]:text-muted-foreground [&_svg:not([class*=\'size-\'])]:size-12 data-[variant=danger]:text-danger-foreground data-[variant=danger]:focus:bg-danger/10 data-[variant=danger]:bg-danger/10 data-[variant=danger]:focus:text-danger-foreground data-[variant=danger]:text-danger-foreground data-[variant=danger]:*:[svg]:!text-danger-foreground dark:data-[variant=danger]:focus:bg-danger/40 dark:data-[variant=danger]:bg-danger/40 relative flex cursor-default items-center p-1 px-2 outline-hidden transition-all select-none focus-visible:ring-0 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+    'duration-0 focus:bg-muted focus:text-accent-foreground text-foreground data-[variant=danger]:text-danger-foreground data-[variant=danger]:focus:bg-danger/10 data-[variant=danger]:bg-danger/10 data-[variant=danger]:focus:text-danger-foreground data-[variant=danger]:text-danger-foreground dark:data-[variant=danger]:focus:bg-danger/40 dark:data-[variant=danger]:bg-danger/40 relative flex cursor-default items-center p-1 px-2 outline-hidden transition-all select-none focus-visible:ring-0 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0',
   ],
 }
 
 const overlayStyles
-  // = 'duration-150 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 fixed inset-0 z-50 bg-overlay backdrop-blur-xs'
-  = 'fixed inset-0 z-50 bg-overlay '
+  = 'fixed inset-0 z-50 bg-overlay'
 
 export { interactiveStyles, overlayStyles, popoverStyles, staticStyles }
